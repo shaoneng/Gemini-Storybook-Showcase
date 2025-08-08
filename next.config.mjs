d02fc71a4e3fc7d29674e67248c964ea27ed186a
shaoneng <wushaoneng@qq.com>
2025-08-08 08:00:18 +0800

    xiugai

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 // 文件路径: /next.config.js
 /** @type {import('next').NextConfig} */
 
-// 首先，默认导入整个配置文件
 import nextI18NextConfig from './next-i18next.config.js';
-
-// 然后，从导入的对象中解构出我们需要的 i18n 部分
 const { i18n } = nextI18NextConfig;
 
 const nextConfig = {
@@ -12,5 +9,4 @@ const nextConfig = {
   i18n,
 };
 
-// 最后，使用 export default 导出配置
 export default nextConfig;
