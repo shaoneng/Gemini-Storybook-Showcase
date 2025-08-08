52a54a74b8f5777a573825285078b1d3c14d3201
shaoneng <wushaoneng@qq.com>
2025-08-08 13:29:21 +0800

    xiugai

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 import '../styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
 import Layout from '../components/Layout';
-// *** 关键修改：导入 i18next 配置文件 ***
+// *** 关键修改 1: 导入 i18next 配置文件 ***
 import nextI18NextConfig from '../next-i18next.config.js';
 
 const MyApp = ({ Component, pageProps }) => {
@@ -13,5 +13,5 @@ const MyApp = ({ Component, pageProps }) => {
   );
 };
 
-// *** 关键修改：将配置文件作为参数传递给 appWithTranslation ***
+// *** 关键修改 2: 将配置文件作为参数传递给 appWithTranslation ***
 export default appWithTranslation(MyApp, nextI18NextConfig);
