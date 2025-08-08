be9753994251bb5499847c0c2e50cdbc3af6dcf8
shaoneng <wushaoneng@qq.com>
2025-08-08 11:53:05 +0800

    xiugai

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-// 文件路径: /next.config.js
+// 文件路径: /next.config.mjs
 /** @type {import('next').NextConfig} */
 
 import nextI18NextConfig from './next-i18next.config.js';
@@ -7,6 +7,8 @@ const { i18n } = nextI18NextConfig;
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  // *** 新增的配置 ***
+  output: 'export', 
 };
 
 export default nextConfig;
